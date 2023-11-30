d3590ad1c8ae3eb4c2968bc5b10c73d4abc024d9
TriqqqS <333qqqsss@gmail.com>
2023-11-30 16:24:52 +0100

    initial commit

---
+++ config/build/buildWebpackConfig.ts
@@ -0,0 +1,31 @@
+import { Configuration } from "webpack";
+import { BuildOptions } from "./types/config";
+import { buildLoaders } from "./buildLoaders";
+import { buildPlugins } from "./buildPlugins";
+import { buildResolvers } from "./buildResolvers";
+import { buildDevServer } from "./buildDevServer";
+
+export function BuildWebpackConfig(options: BuildOptions): Configuration {
+  const { mode, paths, isDev } = options;
+
+  return {
+    mode,
+    entry: paths.entry,
+    output: {
+      filename: "[name].[contenthash].js",
+      path: options.paths.build,
+      clean: true,
+    },
+
+    plugins: buildPlugins(options),
+
+    module: {
+      rules: buildLoaders(options),
+    },
+
+    devtool: isDev ? "inline-source-map" : undefined,
+    devServer: isDev ? buildDevServer(options) : undefined,
+
+    resolve: buildResolvers(),
+  };
+}
